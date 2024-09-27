05c9d6ae1bbc14e11e2e86dbc293620819bfb1a6
reaz24 <reazuddin.dev.bd@gmail.com>
2024-09-27 18:46:29 +0600

    added feature error handling

--- bin/assingment/book.dart
+++ bin/assingment/book.dart
@@ -4,16 +4,12 @@ class Book{
   String _title;
   String _author;
   final int _publicationYear;
-    int _pagesRead =0;
-
+    int _pagesRead;
+    
   //static property to track how many book are created
-
 static int totalBooks = 0 ;
 
-
-
 // constructor
-
 Book({
   required title,
   required author,
@@ -25,41 +21,42 @@ Book({
   _publicationYear = publicationYear,
   _pagesRead = pagesRead{
   totalBooks++ ;// increment each time after book object is created each time
-
+  // Ensure publication year is not in the future
+  if (publicationYear > DateTime.now().year) {
+    throw ArgumentError('Publication year cannot be in the future.');
+  }
+  if (publicationYear < 0 ) {
+    throw ArgumentError('Publication year cannot be Negative ');
+  }
+  // Ensure pages read is not negative
+  if (pagesRead < 0) {
+    throw ArgumentError('Pages read cannot be negative.');
+  }
 }
-
 
 // Get How many pages read and add
 void read(int pages){
+  if (pages < 0) {
+    throw ArgumentError('Number of pages read cannot be negative.');
+  }
   _pagesRead += pages;
-
-
 }
 // return value of pages read
 int getPagesRead(){
   return _pagesRead;
-
 }
 // return title
 String getTitle(){
 return _title;
-
-
-
 }
-
 // return author
 String getAuthor(){
   return _author;
-
 }
-
 // return publication year
 int getPublicationYear(){
   return _publicationYear;
-
 }
-
 // method to calculate of book age
 int getBookAge(){
   int currentYear = DateTime.now().year;
--- bin/assingment/main.dart
+++ bin/assingment/main.dart
@@ -2,9 +2,9 @@ import 'book.dart';
 
 void main(){
 //Create 3 different object for 3 books
-Book book0 =Book(title:'Cash Machine', author:'Koach kanchon',publicationYear: 2028  );
+Book book0 =Book(title:'Cash Machine', author:'Koach kanchon',publicationYear: 2024  );
 Book book1 =Book(title:'Muminer Career Vabna', author:'Dr. Shamsul Arefin Shakti ',publicationYear: 2023  );
-Book book2 =Book(title:'Secrets Of Jainism', author:'Henry Ford ',publicationYear: 2022  );
+Book book2 =Book(title:'Secrets Of Jainism', author:'Henry Ford ',publicationYear: -2022  );
 
 // Simulate reading pages
 
@@ -15,9 +15,9 @@ book2.read(296);
 
 // print details for each books
 
-  print('Book 1: ${book0.getTitle()} by ${book0.getAuthor()}, Published in ${book0.getPublicationYear()}, Pages read: ${book0.getPagesRead()}, Age: ${book0.getBookAge()} years\n\n');
-  print('Book 2: ${book1.getTitle()} by ${book1.getAuthor()}, Published in ${book1.getPublicationYear()}, Pages read: ${book1.getPagesRead()}, Age: ${book1.getBookAge()} years\n\n');
-  print('Book 3 : ${book2.getTitle()} by ${book2.getAuthor()}, Published in ${book2.getPublicationYear()}, Pages read: ${book2.getPagesRead()}, Age: ${book2.getBookAge()} years\n\n');
+  print('Book 1: ${book0.getTitle()} by ${book0.getAuthor()}, Published in ${book0.getPublicationYear()}, Pages read: ${book0.getPagesRead()}, Age: ${book0.getBookAge()} years\n');
+  print('Book 2: ${book1.getTitle()} by ${book1.getAuthor()}, Published in ${book1.getPublicationYear()}, Pages read: ${book1.getPagesRead()}, Age: ${book1.getBookAge()} years\n');
+  print('Book 3 : ${book2.getTitle()} by ${book2.getAuthor()}, Published in ${book2.getPublicationYear()}, Pages read: ${book2.getPagesRead()}, Age: ${book2.getBookAge()} years\n');
 
 
 
